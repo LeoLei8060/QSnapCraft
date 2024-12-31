3a44545ddf3420d0b10f1def4b8df2450d4dce36
LeoLei8060 <18157100608@163.com>
2024-12-31 20:19:23 +0800

    添加iconfont管理器，初始化加载字体

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,23 @@
 #include "utils/SingleApplication.h"
+#include "utils/FontManager.h"
 #include <QMessageBox>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     SingleApplication app(argc, argv, "QSnapCraft");
 
+    // 加载字体文件
+    if (!FontManager::instance()->addThirdpartyFont(":/iconfont/iconfont.ttf", FontManager::IconFont)) {
+        qWarning() << "Failed to load icon font";
+    }
+
     if (app.isRunning()) {
         QMessageBox::warning(nullptr,
-                             "QSnapCraft",
-                             "Another instance of QSnapCraft is already running.");
-        return 1;
+                           "QSnapCraft",
+                           "Application is already running.",
+                           QMessageBox::Ok);
+        return -1;
     }
 
     return app.exec();
