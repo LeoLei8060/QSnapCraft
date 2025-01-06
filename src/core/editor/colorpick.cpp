fb206f0e7c9cdf912a15eb8fe71fa1210a7b5ed7
LeoLei8060 <18157100608@163.com>
2025-01-06 19:03:42 +0800

    完善颜色选择面板功能

--- src/core/editor/colorpick.cpp
+++ src/core/editor/colorpick.cpp
@@ -1,5 +1,6 @@
 #include "colorpick.h"
 #include <QColor>
+#include <QColorDialog>
 #include <QGridLayout>
 #include <QHBoxLayout>
 
@@ -11,12 +12,26 @@ ColorPick::ColorPick(QWidget *parent)
 {
     setWindowFlags(Qt::Widget);
     setupUI();
+    initializeConnect();
 }
 
 ColorPick::~ColorPick() {}
 
+void ColorPick::setColor(const QColor &color)
+{
+    if (color == m_selectColor)
+        return;
+    m_selectColor = color;
+    updateSelectedBtn();
+}
+
 void ColorPick::setupUI()
 {
+    m_colors = QVector<QColor>{"#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00",
+                               "#00FFFF", "#FF00FF", "#FFA500", "#800080", "#A52A2A",
+                               "#FFFFFF", "#808080", "#ADD8E6", "#90EE90", "#FFB6C1",
+                               "#FFFFE0", "#E6E6FA", "#8B0000", "#006400", "#FFC0CB"};
+
     QHBoxLayout *layout = new QHBoxLayout(this);
     layout->setSpacing(2);
     layout->setContentsMargins(1, 1, 1, 1);
@@ -30,13 +45,8 @@ void ColorPick::setupUI()
     gridLayout->setSpacing(2);
     gridLayout->setContentsMargins(1, 1, 1, 1);
 
-    QVector<QColor> colors = {"#000000", "#FF0000", "#00FF00", "#0000FF", "#FFFF00",
-                              "#00FFFF", "#FF00FF", "#FFA500", "#800080", "#A52A2A",
-                              "#FFFFFF", "#808080", "#ADD8E6", "#90EE90", "#FFB6C1",
-                              "#FFFFE0", "#E6E6FA", "#8B0000", "#006400", "#FFC0CB"};
-
     int row = 0, col = 0;
-    for (const QColor &color : colors) {
+    for (const QColor &color : m_colors) {
         QToolButton *btn = new QToolButton(this);
         btn->setFixedSize(12, 12);
         btn->setStyleSheet(QString(BTN_STYLE).arg(color.name()));
@@ -53,4 +63,25 @@ void ColorPick::setupUI()
     }
 
     layout->addLayout(gridLayout);
+}
+
+void ColorPick::initializeConnect()
+{
+    for (int i = 0; i < m_selectColorBtns.count(); ++i) {
+        connect(m_selectColorBtns[i], &QToolButton::clicked, this, [this, i]() {
+            setColor(m_colors[i]);
+        });
+    }
+
+    connect(m_colorBtn, &QToolButton::clicked, this, &ColorPick::onShowColorDialog);
+}
+
+void ColorPick::onShowColorDialog()
+{
+    setColor(QColorDialog::getColor(m_selectColor));
+}
+
+void ColorPick::updateSelectedBtn()
+{
+    m_colorBtn->setStyleSheet(QString(BTN_STYLE).arg(m_selectColor.name()));
 }
--- src/core/editor/colorpick.h
+++ src/core/editor/colorpick.h
@@ -11,10 +11,23 @@ public:
     explicit ColorPick(QWidget *parent = nullptr);
     ~ColorPick();
 
+    QColor getColor() const { return m_selectColor; }
+    void   setColor(const QColor &color);
+
 protected:
     void setupUI();
 
+    void initializeConnect();
+
+private slots:
+    void onShowColorDialog();
+
 private:
+    void updateSelectedBtn();
+
+    QColor m_selectColor{Qt::black};
+
     QToolButton           *m_colorBtn;
     QVector<QToolButton *> m_selectColorBtns;
+    QVector<QColor>        m_colors;
 };
